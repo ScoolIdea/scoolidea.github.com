4a3063ff0d65db9ac6b6933f3b69bd450ea8bb9b
Piotr Marciniak <mandaryyyn@gmail.com>
2014-07-31 17:11:22 +0200

    fixed assets paths

--- config.rb
+++ config.rb
@@ -68,7 +68,7 @@ configure :build do
   # activate :relative_assets
 
   # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  set :http_prefix, "/scool"
 end
 
 activate :deploy do |deploy|
